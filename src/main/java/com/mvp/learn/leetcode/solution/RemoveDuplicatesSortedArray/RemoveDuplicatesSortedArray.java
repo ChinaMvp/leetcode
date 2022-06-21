dfa4cd5ca28b257091411beffd31c2f5d9299cfa
ChinaMvp <china_wangfei@163.com>
2022-06-21 08:44:07 +0800

    add change: add more case

--- src/main/java/com/mvp/learn/leetcode/solution/RemoveDuplicatesSortedArray/RemoveDuplicatesSortedArray.java
+++ src/main/java/com/mvp/learn/leetcode/solution/RemoveDuplicatesSortedArray/RemoveDuplicatesSortedArray.java
@@ -1,5 +1,25 @@
 package com.mvp.learn.leetcode.solution.RemoveDuplicatesSortedArray;
 
+/**
+ *
+ */
 public class RemoveDuplicatesSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if ((i == 0) || (num != nums[i - 1])) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1,2,2,3,4,4,5,6,6};
+        RemoveDuplicatesSortedArray obj = new RemoveDuplicatesSortedArray();
+        int length = obj.removeDuplicates(nums);
+        for (int i = 0; i < length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
 }
